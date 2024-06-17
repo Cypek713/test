581e272485a534e2cfad25d635a90eb487cae47e
Cypek713 <144770080+Cypek713@users.noreply.github.com>
2024-06-17 22:19:31 +0200

    Add files via upload

---
+++ petle2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+int x, y;
+cout << "podaj x:";
+cin >> x;
+cout << "podaj y:";
+cin >> y;
+
+for(int i = x; i<=y; i++){
+    cout << i << endl;
+}
+
+
+
+return 0;
+}
